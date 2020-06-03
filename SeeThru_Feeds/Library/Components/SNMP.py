120580c8dda7bb30494ad3a7b8e3c9afd6a29f8a
JustAidanP <justaidanp@gmail.com>
2020-06-03 15:59:16 +0100

    Added snmp walk component

---
+++ SeeThru_Feeds/Library/Components/SNMP.py
@@ -0,0 +1,51 @@
+from SeeThru_Feeds.Model.Components.ComponentBase import ComponentBase
+from SeeThru_Feeds.Model.Properties.Properties import FillableProperty, ResultProperty
+import pysnmp.hlapi import *
+
+class SNMPWalkToOID(ComponentBase):
+    SNMP_HOST = FillableProperty(name="snmp_host", required=True, ofType=str)
+    SNMP_PORT = FillableProperty(name="snmp_port", default=161, required=True, ofType=int)
+    COMMUNITY = FillableProperty(name="community", required=True)
+
+    VALUE = ResultProperty(name="oid_value")
+
+    Component_Title = "SNMP Walk"
+    Component_Description = "Connects to an SNMP feed, performs an SNMP Walk, and filters it to get the contents within a specified OID value"
+    Component_Author = "SeeThru Networks"
+    Component_Owner = "SeeThru Networks"
+
+    def Component_Execute(self):
+        value = ""
+
+        # For every entry in the SNMP table.
+        for errorIndication, errorStatus, errorIndex, varBinds in bulkCmd(
+                SnmpEngine(),
+                CommunityData(self.GetProperty(self.COMMUNITY)),
+                UdpTransportTarget((self.GetProperty(self.SNMP_HOST), self.GetProperty(self.SNMP_PORT))),
+                ContextData(),
+                0, 50,
+                ObjectType(ObjectIdentity(oid)),
+                maxCalls=10
+        ):
+            # Clear the store for the OID key = value pair.
+            value = ""
+
+            # Do nothing if there was an SNMP error and return the value ""
+            if errorIndication or errorStatus:
+                break
+
+            else:
+                # Otherwise, take the OID key = value pairs and populate the value store.
+                for varBind in varBinds:
+                    value = (' = '.join([x.prettyPrint() for x in varBind]))
+
+                # If the entry is the one we're looking for, then remove the "key = "
+                # prefix from the value string and return the raw value.
+                if search_oid in value:
+                    value = value.replace(search_oid + " = ", "")
+                    break
+
+            # Then move onto the next OID entry in the SNMP feed.
+
+        # Once we call break, the last value will be outputted
+        self.SetProperty(self.VALUE, value)
