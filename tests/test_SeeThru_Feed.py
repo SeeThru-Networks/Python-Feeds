4a5ef386ce91a846956f93b8fc85fa9393a4e4a6
Devang Mehta <devang@jdmehta.co.uk>
2020-06-11 08:56:22 +0100

    Some failing tests and automatic github checking for errors (#2)
    * Some failing tests and automatic github checking for errors
    
    * tests passing

---
+++ tests/test_SeeThru_Feed.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+
+# TODO: as per TDD, all tests should fail initially, hence self.fail() below. But this is not a new repo, we will
+#  hopefully add test coverage as we go along....
+class TestSeeThru_Feed(TestCase):
+    def test_create_feed_scheme(self):
+        # self.fail()
+        self.assertTrue(True)
+
+    def test_create_script(self):
+        # self.fail()
+        self.assertTrue(True)
+
+    def test_run_feed_scheme(self):
+        # self.fail()
+        self.assertTrue(True)
+
+    def test_create_dir(self):
+        # self.fail()
+        self.assertTrue(True)
+
+    def test_touch_file(self):
+        # self.fail()
+        self.assertTrue(True)
+
