68be9f6a16c629773c9048c4ba775583f177f19b
Devang Mehta <devang@jdmehta.co.uk>
2020-06-11 07:26:51 +0100

    Some failing tests and automatic github checking for errors

---
+++ tests/test_SeeThru_Feed.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestSeeThru_Feed(TestCase):
+    def test_create_feed_scheme(self):
+        self.fail()
+
+    def test_create_script(self):
+        self.fail()
+
+    def test_run_feed_scheme(self):
+        self.fail()
+
+    def test_create_dir(self):
+        self.fail()
+
+    def test_touch_file(self):
+        self.fail()
