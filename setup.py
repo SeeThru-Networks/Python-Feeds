db7731d56dbe66026baf8de5914a86b7aade2763
JustAidanP <justaidanp@gmail.com>
2021-03-10 14:32:09 +0000

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,35 @@
 from setuptools import setup, find_packages
 
-setup()
+# setup()
+
+setup(
+    name="seethrufeeds",
+    version="0.0.1",
+    author="SeeThru Networks",
+    author_email="aidan@seethrunetworks.com",
+    description="The SeeThruNetworks feed framework",
+    license="MIT",
+    url="https://github.com/SeeThru-Networks/Python-Feeds",
+    project_urls={
+        "Website": "https://www.seethrunetworks.com/"
+    },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only"
+    ],
+    install_requires=['requests', "python-dotenv", "toml", "mysql-connector-python", "py-zabbix", "pydantic"],
+    python_requires=">=3.9",
+    keywords=["seethru", "seethrunetworks", "feed"],
+    entry_points={
+        "console_scripts": [
+                "SeeThru_Feeds=SeeThru_Feeds.Core.SeeThruFeed:exec",
+                "seethrufeeds=SeeThru_Feeds.Core.SeeThruFeed:exec"
+            ]
+    }
+
+
+)
